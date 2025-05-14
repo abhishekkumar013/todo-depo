ea61b4c584103d27a0e26fed5e581a3c614c38f2
Abhishek kumar <sahaabhishek443@gmail.com>
2025-05-14 14:24:50 +0530

    modify web

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,7 +2,12 @@ import { prismClient } from "@repo/db/client";
 
 export default async function Home() {
   const users = await prismClient.user.findMany();
-  return <div>{JSON.stringify(users)}</div>;
+  return (
+    <div>
+      <h1>User Details</h1>
+      <div>{JSON.stringify(users)}</div>
+    </div>
+  );
 }
 
 // export const revalidate = 60 // revalidate every 60 seconds
